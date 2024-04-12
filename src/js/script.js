c291aebeb077f06d9c34390523d656577a17eaea
kanan <kanansnote@gmail.com>
2024-04-13 02:28:53 +0400

    Almost done, just the responsiveness of the website remains.

--- src/js/script.js
+++ src/js/script.js
@@ -13,15 +13,32 @@ window.addEventListener('scroll', function () {
     }
 });
 
-
-//
-
-
-// Slider for #featuredCategories section using swiper.js
+// Slider for .hotBidsCards section using swiper.js
 const swiper = new Swiper('.hotBidsCards', {
     slidesPerView: '4', // Adjust this value to control how many slides are visible at a time
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+});
+
+//
+const selectForm = document.getElementById('topCollectionsForm');
+const selectDropdown = document.getElementById('topCollectionsDropdown');
+const selectArrowElement = document.querySelector('.fa-angle-down');
+let isOpen = false; // Flag to track open/closed state
+
+selectForm.addEventListener('click', function () {
+  isOpen = !isOpen; // Toggle the flag on each click
+
+  // Update the transform property based on the flag
+  selectArrowElement.style.transform = `rotate(${isOpen ? 180 : 0}deg)`;
+});
+
+document.addEventListener('click', function(event) {
+    if (!event.target.matches('#topCollectionsForm') && !selectDropdown.contains(event.target)) {
+      isOpen = false;
+      selectDropdown.classList.remove('show');
+      selectArrowElement.style.transform = 'rotate(0deg)';
+    }
 });
